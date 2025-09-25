8ee903e6598213161a3be62088fc10a0a6fa544c
LordFoom <lordfoom@gmail.com>
2025-09-25 20:59:20 +0200

    Expanding on errors
    Display messages

--- src/err_types.rs
+++ src/err_types.rs
@@ -1,6 +1,20 @@
+use std::fmt::Display;
+
 #[derive(Debug)]
 pub enum RustyDbErr {
     KeyNotFound(String),
     IoError(std::io::Error),
     SerializationError(String),
 }
+
+impl Display for RustyDbErr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RustyDbErr::KeyNotFound(key) => write!(f, "Key not found: {}", key),
+            RustyDbErr::IoError(error) => write!(f, "IO error: {}", error),
+            RustyDbErr::SerializationError(msg) => write!(f, "Serialization Error: {}", msg),
+        }
+    }
+}
+
+impl std::error::Error for RustyDbErr {}

3e1fa3a0ac1c5960f7211ff2803780b91976c9ce
LordFoom <lordfoom@gmail.com>
2025-10-07 22:09:52 +0200

    some GET parsing
    Buuuut, I'm going to add multi 'table' support first, so pausing here

--- src/command.rs
+++ src/command.rs
@@ -7,7 +7,7 @@ pub enum Command {
 }
 
 ///TODO String or &str?
-pub fn parse(input: &str) -> Result<()> {
+pub fn parse(input: &str) -> Result<Command, ParseError> {
     //split into components (tokens)
     let parts = input.trim().split_whitespace().collect::<Vec<&str>>();
     if parts.is_empty() {
@@ -16,7 +16,17 @@ pub fn parse(input: &str) -> Result<()> {
     //check command is first token
     let command = parts[0].to_uppercase();
     match command.as_str() {
-        "GET" => println!("GET key"),
+        "GET" => {
+            if parts.len() != 2 {
+                return Err(ParseError::WrongNumberOfArguments(format!(
+                    "Get requires 2 arguments,incl. command; you had :{}",
+                    parts.len()
+                )));
+            }
+            return Ok(Command::Get {
+                key: parts[1].to_string(),
+            });
+        }
         "SET" => println!("SET key, val"),
         "DEL" => println!("DEL key"),
         other => {
@@ -28,5 +38,4 @@ pub fn parse(input: &str) -> Result<()> {
 
     //get datastore
     //return value or error if not present
-    Ok(())
 }

6eb2815aba3cb1bf10b12ff1b6d9fa493945477b
LordFoom <lordfoom@gmail.com>
2025-10-10 22:00:29 +0200

    Remove dead code
    Went a different direction

--- src/query.rs
+++
@@ -1,7 +0,0 @@
-///Not going to go this way
-pub enum Query {
-    SELECT { key: Option<String> },
-    INSERT { key: String, val: String },
-    UPDATE { key: String, val: String },
-    DELETE { key: String },
-}
